599c030ddbf606c590a9c180139106db4455a7e1
glbreeze <glbreeze@gmail.com>
2022-06-02 17:07:23 +0800

    fix

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,14 +1,39 @@
+#!/bin/bash
+
+#SBATCH --job-name=seg
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=20GB
+#SBATCH --time=24:00:00
+#SBATCH --mail-type=END
+#SBATCH --mail-user=lg154@nyu.edu
+#SBATCH --output=seg.out
+#SBATCH --gres=gpu # How much gpu need, n is the number
+
+module purge
+
 DATA=$1
 SPLIT=$2
 GPU=$3
 LAYERS=$4
 
-
 dirname="results/train/resnet-${LAYERS}/${DATA}/split_${SPLIT}"
 mkdir -p -- "$dirname"
-python3 -m src.train --config config_files/${DATA}.yaml \
-					 --opts train_split ${SPLIT} \
+
+echo "start"
+singularity exec --nv \
+            --overlay /scratch/lg154/python36/python36.ext3:ro \
+            /scratch/work/public/singularity/cuda11.2.2-cudnn8-devel-ubuntu20.04.sif \
+            /bin/bash -c " source /ext3/env.sh;
+            python -m src.train --config config_files/${DATA}.yaml \
+					  --opts train_split ${SPLIT} \
 						    layers ${LAYERS} \
 						    gpus ${GPU} \
 						    visdom_port 8098 \
-							 | tee ${dirname}/log_${SHOT}.txt
+							 > ${dirname}/log_${SHOT}.txt 2>&1"
+
+echo "finish"
+
+
+#GREENE GREENE_GPU_MPS=yes
